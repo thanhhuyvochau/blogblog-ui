1b5afb657fecc8814c3c351093ccd969d0b452c6
thanhhuyvochau <thanhhuyvochau@gmail.com>
2024-09-16 23:03:49 +0700

    - Add common button component - Set up theme (dark & light) - Set up font

---
+++ src/app/shared/components/blogblog-button/button.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input } from '@angular/core';
+import { SEVERITY } from './blogblog-button.type';
+import { NgIf, NgTemplateOutlet } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [NgTemplateOutlet, NgIf],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent {
+  @Input()
+  severity: SEVERITY = 'primary';
+  @Input()
+  disabled: boolean = false;
+  @Input()
+  onClick: EventEmitter<any> = new EventEmitter();
+  @Input()
+  iconBtn: boolean = false;
+  @Input({ required: true })
+  title: string = '';
+  @Input()
+  fullWidth: boolean = false;
+
+  handleClick(event: any) {
+    this.onClick.emit(this.title);
+  }
+}
